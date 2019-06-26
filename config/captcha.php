2d8c050ca8aa8f70d60f761e56a0462ac0fadc84
oswaldofoxx <52244579+oswaldofoxx@users.noreply.github.com>
2019-06-26 07:41:39 -0500

    Add files via upload

---
+++ config/captcha.php
@@ -0,0 +1,25 @@
+<?php if (!class_exists('CaptchaConfiguration')) { return; } 
+
+// BotDetect PHP Captcha configuration options 
+
+return [ 
+  // Captcha configuration for login page 
+  'LoginCaptcha' => [ 
+    'UserInputID' => 'CaptchaCode', 
+    'CodeLength' => CaptchaRandomization::GetRandomCodeLength(4, 6), 
+    'ImageStyle' => [ 
+      ImageStyle::Radar, 
+      ImageStyle::Collage, 
+      ImageStyle::Fingerprints, 
+    ], 
+  ], 
+  
+  // Captcha configuration for register page 
+  'RegisterCaptcha' => [ 
+    'UserInputID' => 'CaptchaCode', 
+    'CodeLength' => CaptchaRandomization::GetRandomCodeLength(4, 7), 
+    'CodeStyle' => CodeStyle::Alpha, 
+    'CustomLightColor' => '#9966FF', 
+  ], 
+  
+]; 
